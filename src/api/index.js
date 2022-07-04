106474773dac61d61d7d6b8aa42230f8c419f394
Anton Prokopenko <anton.9090@bk.ru>
2022-07-04 13:58:30 +0100

    some initial settings & commit before ejecting

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export default function API() {
+    const params = {
+        baseURL: 'https://aptechnologies.pipedrive.com/v1',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        params: {
+            api_token: '657c1644eb61c4b3980905263ccf3d496d897bfc'
+        }
+    };
+    return axios.create(params);
+}
